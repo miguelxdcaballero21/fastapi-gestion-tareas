35f9b8d1e65fb773ec10fdfe54f97f54f2604234
williamestupinansilva-oss <williamestupinansilva@gmail.com>
2026-08-04 11:24:40 -0500

    migracion a base de datos postgres

--- app/conexion_db.py
+++ app/conexion_db.py
@@ -1,11 +1,10 @@
-import os
-
 from typing import Annotated
 
 from dotenv import load_dotenv
 from fastapi import Depends
+from sqlmodel import Session, SQLModel, create_engine
 
-from sqlmodel import SQLModel, Session, create_engine
+import os
 
 load_dotenv()
 
